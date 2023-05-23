740a35b3e7c1f1e950691802812814cbac002a78
PetterMartin <petter.aa@icloud.com>
2023-05-23 18:04:21 +0200

    pulled API

--- js/app.js
+++ js/app.js
@@ -1,3 +1,106 @@
+/*const homeIcon = document.getElementById("home");
+const aboutIcon = document.getElementById("about");
+const contactIcon = document.getElementById("contact");
+
+
+if (window.location.pathname === "/index.html") {
+  homeIcon.classList.add("active");
+} else if (window.location.pathname === "/about.html") {
+  aboutIcon.classList.add("active");
+} else if (window.location.pathname === "/contact.html") {
+  contactIcon.classList.add("active");
+} */
+
+
+// Load ENV;
+
+const token = import.meta.env.VITE_API_TOKEN;
+
+const url = "https://api.airtable.com/v0/appMZLXTCpDmiDkTh/tblnutZCCuHLWanGx";
+
+const movieContainer = document.querySelector(".hero-2");
+const myHeaders = new Headers();
+myHeaders.append("Authorization", `Bearer ${token}`);
+myHeaders.append(
+  "Cookie",
+  "brw=brwArsjEAWonhZdE8; AWSALB=aM/THwF+1/j4IufjufivnVnS0aaCbjfHnQIePgCobF9H5Nv9v8GTuF18B6XJc3jTpJBwi/fGWdJicXP4U2oiVKXmjbbuM40p5rldMD/uc2uB/wSBLfP1cF7FE+fY; AWSALBCORS=aM/THwF+1/j4IufjufivnVnS0aaCbjfHnQIePgCobF9H5Nv9v8GTuF18B6XJc3jTpJBwi/fGWdJicXP4U2oiVKXmjbbuM40p5rldMD/uc2uB/wSBLfP1cF7FE+fY"
+);
+
+
+const requestOptions = {
+  method: "GET",
+  headers: myHeaders,
+  redirect: "follow",
+};
+
+async function getMovies() {
+  try {
+    const response = await fetch(url, requestOptions);
+    const result = await response.json();
+    const posts = result.records;
+    console.log(posts);
+    if (movieContainer) {
+      let counter = 0;
+      posts.forEach((post) => {
+        const imageContainerClass = counter % 2 === 0 ? 'left-2' : 'right-2';
+        const rightContainerClass = counter % 2 === 0 ? 'right-2' : 'left-2';
+        const postReverse = counter % 2 === 0 ? 'flex-container' : 'flex-reverse';
+
+        movieContainer.innerHTML += `
+          <div class="${postReverse}">
+            <div class="${imageContainerClass}">
+              <div class="image-container">
+                <img src="${post.fields.Attachments[0].url}" alt="Your Image" class="hero-image">
+              </div>
+            </div>
+            <div class="text ${rightContainerClass}">
+              <h1 class="h1">${post.fields["Long Text"]}</h1>
+              <p class="hero-text">Last summer, advertising photographer Jason Perry traveled to Ukraine to provide supplies for civilians amidst the Russian invasion. When travel challenges left him stranded in Amsterdam, his search for ground travel led him to Zeilen Van Vrijheid—a Dutch volunteer team with convoys of ambulances equipped with medical supplies to support doctors in Ukraine.</p>
+              <div class="btn-group">
+                <a href="details.html" class="btn btn-secondary"><span>READ STORY</span></a>
+              </div>
+              <div class="date-category-container">
+                <p class="date">Jan 17, 2022</p>
+                <div class="categories-secondary">
+                  <p>Trailers</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        `;
+
+        counter++;
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getMovies();
+
+/*
+<div class="flex-container">
+                <div class="right-2 right-3">
+                    <h1 class="h1">New Trailer for Christopher Nolan's new movie 'Oppenheimer'</h1>
+                    <p class="hero-text">Last summer, advertising photographer Jason Perry traveled to Ukraine to provide supplies for civilians amidst the Russian invasion. When travel challenges left him stranded in Amsterdam, his search for ground travel led him to Zeilen Van Vrijheid—a Dutch volunteer team with convoys of ambulances equipped with medical supplies to support doctors in Ukraine.</p>
+                   <div class="btn-group">
+                   <a href="details.html" class="btn btn-secondary"><span>READ STORY</span></a>
+                </div>
+                <div class="date-category-container">
+                    <p class="date">Jan 17, 2022</p>
+                    <div class="categories-secondary">
+                    <p>Trailers</p>
+                </div>
+            </div>
+          </div>
+          <div class="left-2">
+            <div class="image-container">
+              <img src="${post.fields.Attachments[0].url}" alt="Your Image" class="hero-image">
+            </div>
+          </div>
+          </div>
+
 /*
 ============================================
 Constants
@@ -51,3 +154,5 @@ https://github.com/S3ak/fed-javascript1-api-calls/blob/main/examples/games.html#
  * @example https://github.com/S3ak/fed-javascript1-api-calls/blob/main/src/js/detail.js#L36
  * @param {item} item The object with properties from the fetched JSON data.
  */
+
+
--- js/details.js
+++ js/details.js
@@ -1,3 +1,66 @@
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+const heroContainer = document.querySelector(".hero");
+
+const token = import.meta.env.VITE_API_TOKEN;
+
+
+const myHeaders = new Headers();
+myHeaders.append("Authorization", `Bearer ${token}`);
+myHeaders.append("Cookie", "brw=brwArsjEAWonhZdE8; AWSALB=aM/THwF+1/j4IufjufivnVnS0aaCbjfHnQIePgCobF9H5Nv9v8GTuF18B6XJc3jTpJBwi/fGWdJicXP4U2oiVKXmjbbuM40p5rldMD/uc2uB/wSBLfP1cF7FE+fY; AWSALBCORS=aM/THwF+1/j4IufjufivnVnS0aaCbjfHnQIePgCobF9H5Nv9v8GTuF18B6XJc3jTpJBwi/fGWdJicXP4U2oiVKXmjbbuM40p5rldMD/uc2uB/wSBLfP1cF7FE+fY");
+
+const requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+const url = await fetch('https://api.airtable.com/v0/appMZLXTCpDmiDkTh/tblnutZCCuHLWanGx', requestOptions);
+
+async function getDetails() {
+  try {
+    const response = await fetch(url + `/${id}`, requestOptions);
+    const result = await response.json();
+    console.log(result)
+    const movie = result.find(obj => obj.id == id)
+    console.log(movie)
+
+    heroContainer.innerHTML = `
+    <div class="hero-container">     
+              <div class="left">      
+                <h1 class="h1">New Trailer for Christopher Nolans new movie 'Oppenheimer'</h1>    
+              </div>
+    </div>
+
+    <div class="text-section">
+            <div class="text-container">
+            </div>
+        </div>
+
+        <div class="image-section">
+            <div class="image-container">
+                <img src="" alt="">
+            </div>
+        </div>
+
+        <div class="text-section">
+            <div class="text-container">
+                <p>
+                    The trailer showcases the exceptional performances of the cast, led by a mesmerizing portrayal of Oppenheimer by the talented actor at the center of the film. The intense and thought-provoking dialogue serves as a reminder of the weighty decisions and profound implications that Oppenheimer and his colleagues grappled with throughout their groundbreaking scientific endeavor.<br>
+                </p>
+            </div>
+        </div>
+      `;
+  } catch (error) {
+    heroContainer.innerHTML = `error; ${error}`;
+  }
+}
+
+if (id) {
+  getDetails();
+}
+
 /*
 ============================================
 Constants
